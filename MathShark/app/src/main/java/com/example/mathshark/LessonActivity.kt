56a39043be36326c06177edbe5af779f8b380758
Jefferson valero <141515255+Angelus127@users.noreply.github.com>
2024-04-03 16:56:37 -0500

    Add files via upload
    navagacion por la aplicacion desde el listado de la lecciones hasta el quiz (listado de lecciones, leccion y quiz por leccion)

---
+++ MathShark/app/src/main/java/com/example/mathshark/LessonActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mathshark
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mathshark.databinding.ActivityLessonBinding
+import com.example.mathshark.ui.QuestionActivity
+
+class LessonActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLessonBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLessonBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.quizButton.setOnClickListener(){
+            val i = Intent(this, QuestionActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
---
+++ MathShark/app/src/main/java/com/example/mathshark/LessonListActivity.kt
@@ -0,0 +1,45 @@
+package com.example.mathshark
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.mathshark.databinding.ActivityLessonListBinding
+import com.example.mathshark.ui.MyAdapter
+import java.util.Scanner
+
+class LessonListActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLessonListBinding
+    val lessonInfo = mutableListOf<MutableList<String>>()
+
+    val topics = arrayListOf<String>("Ecuacion Lineal", "Ecuacion Cuadratica")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLessonListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.titleTopic.text = "Ecuaciones"
+
+        readLessonInfo()
+
+        val adapter = MyAdapter(this, lessonInfo)
+        binding.lessonList.adapter = adapter
+    }
+
+
+
+
+    fun readLessonInfo() {
+        val input = Scanner(resources.openRawResource(R.raw.lessoninfo))
+        var count = 0
+        while (input.hasNextLine()) {
+            val data = input.nextLine().split("|")
+            lessonInfo.add(data.toMutableList())
+            count += 1
+        }
+        input.close()
+    }
+
+}
